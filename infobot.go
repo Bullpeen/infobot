09e7bf0227e2cea2657cf48915bcf77636d1976d
Justin Gallardo <justin.gallardo@gmail.com>
2017-06-12 23:44:39 -0700

    Add initial infobot code.

---
+++ infobot.go
@@ -0,0 +1,68 @@
+package infobot
+
+import (
+	"context"
+
+	"github.com/jirwin/factpacks"
+	"github.com/jirwin/quadlek/quadlek"
+
+	"fmt"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+var factStore = factpacks.MakeFactStore()
+
+const FactStoreKey = "facts"
+
+func load(bot *quadlek.Bot, store *quadlek.Store) error {
+	return store.Get(FactStoreKey, func(rec []byte) error {
+		return factStore.Load(rec)
+	})
+}
+
+func infobot(ctx context.Context, hookChan <-chan *quadlek.HookMsg) {
+
+	for {
+		select {
+
+		case hookMsg := <-hookChan:
+			if !hookMsg.Bot.MsgToBot(hookMsg.Msg.Text) {
+				continue
+			}
+
+			line := strings.TrimPrefix(hookMsg.Msg.Text, fmt.Sprintf("<@%s> ", hookMsg.Bot.GetUserId()))
+
+			factStore.HumanProcess(line)
+			out, err := factStore.Serialize()
+			if err != nil {
+				log.WithField("err", err).Error("Error serializing factstore.")
+				continue
+			}
+
+			err = hookMsg.Store.Update(FactStoreKey, out)
+			if err != nil {
+				log.WithField("err", err).Error("Error while saving factstore.")
+				continue
+			}
+
+		case <-ctx.Done():
+			log.Info("Shutting down infobot hook.")
+			return
+		}
+	}
+}
+
+func Register() quadlek.Plugin {
+	return quadlek.MakePlugin(
+		"infobot",
+		nil,
+		[]quadlek.Hook{
+			quadlek.MakeHook(infobot),
+		},
+		nil,
+		nil,
+		load,
+	)
+}
